78ca98bd777e05c5030bddfc906d33cd66eb27e6
hvtienprotv84 <dmx031117@gmail.om>
2024-07-18 07:30:02 +0700

    push

---
+++ README.md
@@ -0,0 +1,9 @@
+# First CRUD - Django - Python - MySQL
+- Django
+- Python
+- MySQL
+# RUN
+- Bước 1: `pip install Django`
+- Bước 2: `pip install mysqlclient`
+- Bước 3: `python manage.py migrate`
+- Bước 4: `python manage.py runserver`
---
+++ newapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Member(models.Model):
+    firstname=models.CharField(max_length=100)
+    lastname=models.CharField(max_length=100)
+    country=models.CharField(max_length=100)
+
+class Member1(models.Model):
+    firstname=models.CharField(max_length=100)
+    lastname=models.CharField(max_length=100)
+    country=models.CharField(max_length=100)
